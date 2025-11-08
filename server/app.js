52d7c1047bd3612638d3afe95ca57c08cd6af0b7
Vicente Bercasio <vicentetobio00@gmail.com>
2025-11-08 18:02:12 +0800

    working locations CRUD

--- server/app.js
+++ server/app.js
@@ -1,29 +1,28 @@
-// app.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const logger = require('./src/utils/logger.js');
+const mainRouter = require('./src/routes/router.js'); 
 
-
-// Load environment variables
 dotenv.config();
 
-// Initialize the Express application
 const app = express();
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 app.disable('x-powered-by');
 
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api', mainRouter); 
+
+app.get('/', (req, res) => {
+    res.send('🌍 Earthquake Safety API is running...');
+});
 
 app.use((req, res) => {
-  logger.warn('Route not found', {
-    url: req.url,
-    method: req.method,
-    ip: req.ip
-  });
-
-  res.status(404).json({
-    success: false,
-    message: 'Route not found'
-  });
+    logger.warn('Route not found', { url: req.url, method: req.method, ip: req.ip });
+    res.status(404).json({ success: false, message: 'Route not found' });
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const logger = require('./src/utils/logger.js');
-
+const router = require( './src/routes/router.js');
+const express = require('express');
 
 dotenv.config();
+app.use(express.json());
 
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.send('🌍 Earthquake Safety API is running...');
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
